52896e236ccbd1a1e835e2808f46492a4a3b92e6
Roy <satriaroy70@gmail.com>
2025-09-13 21:14:08 +0700

    chore: add libraries

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,24 @@
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
 import apiRoutes from "./api/index.js";
+import logger from "./utils/logger.js";
+import { pinoHttp } from "pino-http";
+import { errorHandler } from "./middleware/errorHandler.js";
 
-dotenv.config();
 const app: Express = express();
 
+// Use logger
+app.use(pinoHttp({ logger }));
+// Json parser
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Test API");
 });
 
+// Routes
 app.use("/api/v1", apiRoutes);
+
+// Error middleware
+app.use(errorHandler);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
 import app from "./app.js";
+import logger from "./utils/logger.js";
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`[server]: Server is running at port:${port}`);
+    logger.info(`Server is running at port:${port}`);
 });
