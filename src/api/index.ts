1e3225633a6956e573c8fcd2b88468513caa2a70
Roy <satriaroy70@gmail.com>
2025-11-15 12:29:52 +0700

    feat: add invitations and templates to index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,13 +2,17 @@ import { Router } from "express";
 import userRoutes from "./users/user.routes.js";
 import authRoutes from "./auth/auth.routes.js";
 import transactionRoutes from "./transactions/transaction.routes.js";
+import invitationRoutes from "./invitations/invitation.routes.js";
 import webhookRoutes from "./webhooks/webhook.routes.js";
+import templateRoutes from "./templates/template.routes.js";
 
 const router = Router();
 
 router.use("/users", userRoutes);
 router.use("/auth", authRoutes);
 router.use("/transactions", transactionRoutes);
+router.use("/invitations", invitationRoutes);
 router.use("/webhooks", webhookRoutes);
+router.use("/templates", templateRoutes);
 
 export default router;
